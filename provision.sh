efe9cfb86c233a47852e355652614f8a14e48b3e
Renee Ju <renee.ju01@gmail.com>
2022-05-23 13:12:33 -0700

    initial vagrant 2022

--- provision.sh
+++ provision.sh
@@ -9,7 +9,7 @@ cd /vagrant
 sudo apt-get update
 sudo apt-get install tree
 
-# 安装配置mysql8
+# Install mysql8
 if ! [ -e /vagrant/mysql-apt-config_0.8.15-1_all.deb ]; then
 	wget -c https://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb
 fi
@@ -23,58 +23,37 @@ if [ ! -f "/usr/bin/pip" ]; then
   sudo apt-get install -y python-setuptools
   sudo ln -s /usr/bin/pip3 /usr/bin/pip
 else
-  echo "pip3 已安装"
+  echo "pip3 already installed"
 fi
 
-# 升级pip，目前存在问题，read timed out，看脸，有时候可以，但大多时候不行
+# Upgrade pip, it has some issues
 # python -m pip install --upgrade pip
-# 换源完美解决
-# 安装pip所需依赖
+## Incase there's issues reinstall pip
+## Install pip dependencies
 pip install --upgrade setuptools
 pip install --ignore-installed wrapt
-# 安装pip最新版
+## Install the latest pip
 pip install -U pip
-# 根据 requirements.txt 里的记录安装 pip package，确保所有版本之间的兼容性
+# Install packages, ensure compatibility between all versions
 pip install -r requirements.txt
 
 
-# 设置mysql的root账户的密码为yourpassword
-# 创建名为twitter的数据库
+# set mysql user root's password
+# create a new database twitter
 sudo mysql -u root << EOF
-	ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'yourpassword';
+	ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY'cosmo';
 	flush privileges;
 	show databases;
 	CREATE DATABASE IF NOT EXISTS twitter;
 EOF
 # fi
 
-# superuser名字
-USER="admin"
-# superuser密码
-PASS="admin"
-# superuser邮箱
-MAIL="admin@twitter.com"
-script="
-from django.contrib.auth.models import User;
 
-username = '$USER';
-password = '$PASS';
-email = '$MAIL';
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password);
-    print('Superuser created.');
-else:
-    print('Superuser creation skipped.');
-"
-printf "$script" | python manage.py shell
-
-
-# 如果想直接进入/vagrant路径下
-# 请输入vagrant ssh命令进入
-# 手动输入
-# 输入ls -a
-# 输入 vi .bashrc
-# 在最下面，添加cd /vagrant
+# if you want to go to /vagrant
+# enter vagrant ssh
+# enter manually
+# enter ls -a
+# enter vi .bashrc
+# add cd /vagrant
 
 echo 'All Done!'
--- utils/time_helpers.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-import pytz
-
-
-def utc_now():
-    return datetime.now(tz=pytz.utc)
